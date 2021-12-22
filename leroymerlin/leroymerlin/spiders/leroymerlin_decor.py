33fe161bfb195140b5d9245c15fab032d9e1888a
gnosheva <alenagnosheva@gmail.com>
2021-12-22 04:32:55 +0400

    Parsing

--- leroymerlin/leroymerlin/spiders/leroymerlin_decor.py
+++ leroymerlin/leroymerlin/spiders/leroymerlin_decor.py
@@ -5,7 +5,7 @@ import json
 class LeroymerlinDecorSpider(scrapy.Spider):
     name = 'leroymerlin_decor'
     allowed_domains = ['leroymerlin.ru']
-    start_urls = ['https://leroymerlin.ru/catalogue/dekor/?page=918']
+    start_urls = ['https://leroymerlin.ru/catalogue/dekor/?page=1']
     custom_settings = {'FEED_URI': "leroymerlin_%(time)s.json",
                        'FEED_FORMAT': 'json',
                        'FEED_EXPORT_ENCODING' : 'utf-8'}
@@ -20,40 +20,18 @@ class LeroymerlinDecorSpider(scrapy.Spider):
         price_range = response.css('.t3y6ha_plp.xc1n09g_plp.p1q9hgmc_plp::text').extract()
         for i in range(0, len(price_range)):
             price_range[i] = price_range[i].replace('\xa0', '')
-        # Извлечение данных с использованием xpathscrapy crawl leroymerlin_decor
-        # orders = response.xpath("//em[@title='Total Orders']/text()").extract()
-        # company_name = response.xpath("//a[@class='store $p4pLog']/text()").extract()
-
         row_data = zip(product_name, price_range)
 
 
         # извлечение данных строки
         for item in row_data:
-            # создать словарь для хранения извлеченной информации
-
             scraped_info = {
                 # 'id': self.id,
                 'page': response.url,
                 'product_name': item[0],
-                # item[0] означает продукт в списке и т. д., индекс указывает, какое значение назначить
                 'price_range': item[1],
-                # 'orders': item[2],
-                # 'company_name': item[3],
             }
             yield scraped_info
-            # self.id+=1
-        # next_page = response.css('.bex6mjh_plp.o1ojzgcq_plp.l7pdtbg_plp.r1yi03lb_plp.sj1tk7s_plp::attr(href)').get()
-        # if next_page != None and self.flag < 3:
-        #     yield scrapy.Request(
-        #         response.urljoin(next_page),
-        #         callback=self.parse)
-        #     self.flag += 1
-
-        # NEXT_PAGE_SELECTOR = 'a.bex6mjh_plp.s15wh9uj_plp.l7pdtbg_plp.r1yi03lb_plp.sj1tk7s_plp::attr(href)'
-
-
-        # class ="bex6mjh_plp s15wh9uj_plp l7pdtbg_plp r1yi03lb_plp sj1tk7s_plp"
-
         next_page = (response.css('.bex6mjh_plp.s15wh9uj_plp.l7pdtbg_plp.r1yi03lb_plp.sj1tk7s_plp::attr(href)').extract())[-1]
 
         print('ФЛАГ', (self.flag), type(next_page), next_page)
@@ -63,12 +41,6 @@ class LeroymerlinDecorSpider(scrapy.Spider):
                     callback=self.parse)
                 self.flag += 1
 
-          # yield response.follow(next_page,callback=self.parse)
-
-
-
-            # генерируем очищенную информацию для скрапа
-            # yield scraped_info
 # scrapy crawl leroymerlin_decor
 
-# bex6mjh_plp.o1ojzgcq_plp.l7pdtbg_plp.r1yi03lb_plp.sj1tk7s_plp
+
